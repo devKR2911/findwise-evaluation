31f20cb4c8e7d4779590498699b708602cbe383a
Nitheesh KR <nitheeshkr115@gmail.com>
2022-04-07 18:23:01 +0530

    Created a button component and added to home screen

---
+++ src/components/core/Button/Button.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <button>{{ label }}</button>
+    </div>
+</template>
+<script lang="ts">
+
+import { defineComponent } from "vue";
+
+export default defineComponent({
+    name: "CButton",
+    props: {
+        label: {
+            type: String,
+            required: false,
+            default: "",
+        }
+    },
+    setup() {
+        // props, {emit}
+        return {
+
+        }
+    }
+})
+</script>
+
+<style lang="scss">
+
+</style>
